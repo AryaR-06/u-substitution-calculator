04ccd99ef86f26da5699d4a05018cd624364be1d
Arya Ranadive <83094474+AryaR-06@users.noreply.github.com>
2025-01-05 15:59:44 -0500

    Update app.py

--- website/app.py
+++ website/app.py
@@ -136,4 +136,4 @@ def solve():
         return jsonify({"error": f"Server Error: Please try again later."}), 500
     
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=True)
